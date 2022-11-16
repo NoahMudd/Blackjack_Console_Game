a4ecaa1b844a417b7e13a1e0548c7e9b9ea28bf4
Noah Mudd <noah.mudd@outlook.com>
2022-11-16 12:24:22 -0500

    untracked files

---
+++ Blackjack_ConsoleApp/AboutGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_ConsoleApp
+{
+    public class AboutGame
+    {
+        public static int DisplayAbout()
+        {
+            {
+                   
+                Console.WriteLine("Blackjack Console Game");
+                Console.WriteLine("Blackjack is a card game where the goal is to get");
+                Console.WriteLine("closer to 21 than the dealer, without going over.");
+                Console.WriteLine("You'll place a bet before your cards are dealt.");
+                Console.WriteLine("Next, you'll recieve two cards. You can either 'hit'");
+                Console.WriteLine("or 'stand' to either receive another card, or pass.");
+                Console.WriteLine("The dealer will then do the same, revealing the winner.");
+                Console.WriteLine("at the end.");
+                Console.WriteLine("");
+                Console.WriteLine("Other Considerations");
+                Console.WriteLine("-If you beat the dealer you win back double what you bet.");
+                Console.WriteLine("-If your card total is exactly 21 you immediately win.");
+                Console.WriteLine("-If you tie the dealer it's a 'push', your bet is returned.");
+                
+                Console.WriteLine("--------------------------------------------------");
+
+                Console.WriteLine("Press 2 and hit Enter to return to main menu");
+                int selectedOption = UtilitySave.ReturnToMainMenu();
+                return selectedOption;
+            }
+
+        }
+    }
+}
---
+++ Blackjack_ConsoleApp/UtilityBets.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_ConsoleApp
+{
+    public class UtilityBets
+    {
+        public int playerBet;
+
+        public int playerBetReturn;
+    
+    //public static int BetIntake(int playerBet, bool winOrLose)
+       // {
+
+       // }
+    
+    public static List<Player> DidPlayerWin(int handTotal, int dealerHandTotal, List<Player>player1Profile)
+        {
+            if (handTotal > dealerHandTotal && handTotal <= 21)
+            {
+                Console.WriteLine("Congrats, you won!");
+                int playerWinnings = player1Profile[0].playerBet + player1Profile[0].playerBet;
+                int playerNewChipCount = player1Profile[0].chipCount + playerWinnings;
+                player1Profile[0].chipCount = playerNewChipCount;
+            }
+            else if (handTotal == 21)
+            {
+                Console.WriteLine("Congrats, you won!");
+                int playerWinnings = player1Profile[0].playerBet + player1Profile[0].playerBet;
+                int playerNewChipCount = player1Profile[0].chipCount + playerWinnings;
+                player1Profile[0].chipCount = playerNewChipCount;
+            }
+            else if (handTotal == dealerHandTotal && handTotal < 21)
+            {
+                Console.WriteLine("You tied the dealer! Your bet is returned.");
+                int playerNewChipCount = player1Profile[0].chipCount + player1Profile[0].playerBet;
+                player1Profile[0].chipCount = playerNewChipCount;
+            }
+            else if (handTotal <= 21 && dealerHandTotal > 21)
+            {
+                Console.WriteLine("The dealer busted, you won!");
+                int playerWinnings = player1Profile[0].playerBet + player1Profile[0].playerBet;
+                int playerNewChipCount = player1Profile[0].chipCount + playerWinnings;
+                player1Profile[0].chipCount = playerNewChipCount;
+            }
+
+            else
+            {
+                Console.WriteLine("You lost!");
+            }
+
+
+
+            return player1Profile;
+            
+            
+        }
+    
+        public static List<Player> TakePlayerBet(List<Player> player1Profile, int player1Chips)
+        {
+            Console.WriteLine("What would you like to bet?");
+            Console.WriteLine("You have: " + player1Profile[0].chipCount);
+            Console.WriteLine("Enter numerial input less than or equal to your current chips: ");
+            int playerBet = Int32.Parse(Console.ReadLine());
+            while(playerBet > player1Profile[0].chipCount || playerBet <= 0)
+            {
+                Console.WriteLine("Invalid bet amount. Try again: ");
+                playerBet = Int32.Parse(Console.ReadLine());
+            }
+            int newChipCount = player1Profile[0].chipCount - playerBet;
+            player1Profile[0].chipCount = newChipCount;
+            player1Profile[0].playerBet = playerBet;
+            return player1Profile;
+        }
+    
+    
+    
+        public static int IsChipCountZero(List<Player> player1Profile)
+        {
+            if (player1Profile[0].chipCount == 0)
+            {
+                //Console.Clear();
+                Console.WriteLine("You  are out of chips. Returning to main menu.");
+                int singlePlayerSelectedOption = 2;
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+                return singlePlayerSelectedOption;
+            }
+            else
+            {
+                int singlePlayerSelectedOption = 9;
+                return singlePlayerSelectedOption;
+            }
+
+        }
+    
+    
+    }
+}
